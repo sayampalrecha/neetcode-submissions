ea7017b01520d7ce5aed840e0686852b7742e576
Sayam Palrecha <104487341+sayampalrecha@users.noreply.github.com>
2026-07-05 18:40:38 -0400

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.rs
@@ -0,0 +1,16 @@
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut seen = HashMap::new();
+        
+        for (i, &num) in nums.iter().enumerate(){
+            let diff = target - num;
+
+            if let Some(&prev_index) = seen.get(&diff) {
+                return vec![prev_index as i32, i as i32];
+            }
+            seen.insert(num,i);
+
+        }
+        vec![]
+    }
+}
