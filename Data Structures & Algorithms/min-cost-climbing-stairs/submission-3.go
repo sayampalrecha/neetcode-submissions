11c4a36f028ff608468ccf38a995c08f8e28cef6
Sayam Palrecha <104487341+sayampalrecha@users.noreply.github.com>
2026-07-08 21:11:41 -0400

    Add: min-cost-climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-3.go
@@ -0,0 +1,12 @@
+// package main
+
+func minCostClimbingStairs(cost []int) int {
+    downOne := 0
+    downTwo := 0
+    for i := 2; i<= len(cost); i++ {
+        current := min(downOne+cost[i-1],downTwo+cost[i-2])
+        downTwo = downOne
+        downOne = current
+    }
+    return downOne
+}
