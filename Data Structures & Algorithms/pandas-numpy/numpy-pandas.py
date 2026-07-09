090c10bffda95afdf105accfdfa6bbc66f73293f
Sayam Palrecha <sayampalrecha09@gmail.com>
2026-07-08 20:56:01 -0400

    pandas

---
+++ Data Structures & Algorithms/pandas-numpy/numpy-pandas.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# Create a 1D array from a list
+arr_1d = np.array([1, 2, 3, 4, 5])
+
+# Create a 2D array (a matrix) from a list of lists
+arr_2d = np.array([[1, 2, 3], [4, 5, 6]])
+
+# Built-in methods for quick creation
+zeros = np.zeros(5)        # [0., 0., 0., 0., 0.]
+ones = np.ones((2, 2))     # 2x2 matrix of 1s
+range_arr = np.arange(10)  # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
