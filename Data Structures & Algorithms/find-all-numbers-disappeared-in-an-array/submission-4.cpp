74b434f2162797ed79019a6075fe463e4d264f02
Sayam Palrecha <104487341+sayampalrecha@users.noreply.github.com>
2026-07-10 23:55:01 -0400

    Add: find-all-numbers-disappeared-in-an-array - submission-4

---
+++ Data Structures & Algorithms/find-all-numbers-disappeared-in-an-array/submission-4.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+#include <cmath>
+
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        std::sort(nums.begin(), nums.end());
+
+        int n = nums.size();
+
+        std::vector<int> res;
+
+        for (int i =1; i <= n; ++i) {
+
+            if (std::find(nums.begin(), nums.end(), i) == nums.end()) {
+                res.push_back(i);
+            }
+        }
+        return res;
+    }
+};
