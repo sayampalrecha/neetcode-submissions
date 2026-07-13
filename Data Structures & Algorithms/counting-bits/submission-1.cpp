b83254d4abc4c3fb5dada5ca0d121452de668d25
Sayam Palrecha <104487341+sayampalrecha@users.noreply.github.com>
2026-07-13 00:44:45 -0400

    Add: counting-bits - submission-1

---
+++ Data Structures & Algorithms/counting-bits/submission-1.cpp
@@ -0,0 +1,12 @@
+#include <vector>
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        std::vector<int> res(n+1,0);
+        for (int i=1;i<=n; ++i) {
+            res[i] = res[i >> 1] + (i& 1);
+        }
+        return res;
+        
+    }
+};
