81413bef7fbad23336c86a7fa7b82c4687094633
Sayam Palrecha <104487341+sayampalrecha@users.noreply.github.com>
2026-07-16 23:15:47 -0400

    Implement BFS using a queue in bfs.py
    Added a new BFS implementation using a queue.

--- Data Structures & Algorithms/Dijkstra's-Astar-BFS-DFS-Prims/bfs.py
+++ Data Structures & Algorithms/Dijkstra's-Astar-BFS-DFS-Prims/bfs.py
@@ -25,3 +25,18 @@ def bfs(visited, graph, node):
 
 print("Following is the Breadth-First Search")
 bfs(visited, graph, '5')
+
+
+# ------------- # ------------ # --------------#
+
+from collections import deque
+
+def bfs_by_queue(root):
+  queue = deque([root]) # atleast one element in the queue to kick start bfs
+  while len(queue) > 0: # aslong as there is an element in the queue
+    node = queue.popleft() # dequeue
+    for child in node.children: # enqueue children
+      if OK(child): # early return if problem condition met
+        return FOUND(child)
+      queue.append(child)
+  return NOT_FOUND
